1e52bbf4c7e4766299f1e2f581f0adc108d2969d
Brian Chu <chu.brianm@gmail.com>
2022-02-20 20:09:39 -0500

    Style mobile menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,16 +2,19 @@ import Nav from './Nav';
 import OpenMenuIcon from '../images/Open Menu Icon.svg';
 
 const Header = () => {
+    const toggleMenu = () => {
+        console.log( 'open menu' );
+    }
     return (
         <header>
             <div className='masthead bg-royal-blue text-white px-2 py-8 relative overflow-x-hidden'>
-                <div className='bg-outer-circle bg-white opacity-30 rounded-full absolute -right-20 -top-20 grid content-center -z-50'>
+                <div className='bg-outer-circle bg-white opacity-30 rounded-full absolute -right-20 -top-20 grid content-center z-30 md:-z-20'>
                     <div className='bg-inner-circle rounded-full bg-royal-blue inline-block mx-auto'></div>
                 </div>
                 <div className='bg-lines--vertical absolute left-0 right-0 top-0 bottom-0 m-auto'></div>
                 <div className='container max-w-screen-lg mx-auto flex items-center justify-between relative z-1'>
-                    <a href="/" className='font-semibold text-xl tracking-tight py-2'>Rideaway</a>
-                    <button className='md:hidden'><img src={OpenMenuIcon} /></button>
+                    <a href="/" className='font-semibold text-xl py-2 relative z-50'>Rideaway</a>
+                    <button className='relative z-50 md:hidden' onClick={toggleMenu}><img src={OpenMenuIcon} /></button>
                     <Nav />
                 </div>
             </div>
--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -4,16 +4,16 @@ const Nav = () => {
         ['Features','/'],
         ['Compare Plans','/'],
         ['Contact Us','/']
-    ].map(([title, url]) => (<a href={url} className="hover:underline">{title}</a>));
+    ].map(([title, url]) => (<li key={title} className='my-5'><a href={url} className="text-white hover:underline">{title}</a></li>));
 
     return (
-        <nav className='hidden md:block'>
-            <ul className='ml-auto flex items-center space-x-4'>
+        <nav className='hidden mobile-nav md:block relative z-20 md:z-50'>
+            <ul className='px-2 py-8 md:px-0 md:py-0 md:ml-auto md:flex md:flex-row md:items-center md:space-x-4'>
 
                 { navigationLinks }
 
-                <li className="inline-block">
-                    <a href="google.com" className='inline-block px-7 py-1 rounded-full border-2 border-white bg-white text-royal-blue font-semibold hover:bg-transparent hover:text-white hover:no-underline ease-in-out duration-100'>Free Demo</a>
+                <li className="relative z-50">
+                    <a href="google.com" className='inline-block w-full px-7 py-2 mt-10 md:mt-0 text-center rounded-lg border-2 border-white bg-white text-royal-blue font-semibold hover:bg-transparent hover:text-white hover:no-underline ease-in-out duration-100'>Free Demo</a>
                 </li>
             </ul>
         </nav>
