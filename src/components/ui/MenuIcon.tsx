f58f5a26b907f7d011da87f2e0c19073f5b960e6
Brian Chu <chu.brianm@gmail.com>
2022-02-20 23:12:20 -0500

    Apply cleanup function

--- src/components/ui/MenuIcon.tsx
+++ src/components/ui/MenuIcon.tsx
@@ -9,13 +9,31 @@ const MenuIcon = () => {
         menuIcon: OpenMenuIcon,
     });
 
-    useEffect(() => {
-        menu.isOpen === true ? document.body.classList.add('open-menu') : document.body.classList.remove('open-menu');
-    })
-
     const toggleMenuHandler = () => {
         menu.isOpen === false ? toggleMenu({ ...menu, isOpen: true, menuIcon: CloseMenuIcon }) : ( menu.isOpen === true ? toggleMenu({ ...menu, isOpen: false, menuIcon: OpenMenuIcon } ) : toggleMenu({ ...menu, isOpen: true, menuIcon: CloseMenuIcon }) );
     }
+
+    const [size, setSize] = useState(window.innerWidth);
+    
+    const checkSize = () => {
+        setSize(window.innerWidth);
+    }
+
+    useEffect(() => {
+        if ( menu.isOpen === true ) {
+            document.body.classList.add('open-menu');
+        }
+        if ( size > 768 ) {
+            document.body.classList.remove('open-menu');
+        }
+
+        window.addEventListener('resize', checkSize);
+        
+        return () => {
+            document.body.classList.remove('open-menu');
+            window.removeEventListener('resize', checkSize);
+        }
+    })
 
     return <button className='relative z-50 md:hidden' onClick={toggleMenuHandler}><img src={menu.menuIcon} /></button>
 }
