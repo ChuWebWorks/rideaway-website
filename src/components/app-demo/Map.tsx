c70cb7a59cd3df769c08229994112038cdd7dda5
Brian Chu <chu.brianm@gmail.com>
2022-02-25 12:07:57 -0500

    Add mapbox geocoder

--- src/components/app-demo/Map.tsx
+++ src/components/app-demo/Map.tsx
@@ -1,7 +1,13 @@
 import React, { useState, useEffect, useRef } from 'react';
-import 'mapbox-gl/dist/mapbox-gl.css';
 import mapboxgl from 'mapbox-gl';
+import MapboxGeocoder from '@mapbox/mapbox-gl-geocoder';
 
+// Stylesheets.
+import 'mapbox-gl/dist/mapbox-gl.css';
+import '@mapbox/mapbox-gl-geocoder/dist/mapbox-gl-geocoder.css';
+
+// Images.
+import MenuIcon from '../../images/App Demo Menu.svg';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoiZ2VudGxlbWluaCIsImEiOiJjbDAwZmx4Z3QwNmZlM3BvZGR3eTc0YjNnIn0._Sfl9X0gUdZ_XY_MlFt6_g';
 
@@ -10,7 +16,8 @@ const Map = () => {
     const map = React.useRef<mapboxgl.Map | null>(null);
     const [lng, setLng] = useState(-70.25689);
     const [lat, setLat] = useState(43.6591);
-    const [zoom, setZoom] = useState(10);
+    const [zoom, setZoom] = useState(12);
+    const [position, setPosition] = useState('top-left');
     
     // Creates the map.
     useEffect( () => {
@@ -23,27 +30,42 @@ const Map = () => {
             zoom: zoom
         });
 
-        map.current.addControl(
-            new mapboxgl.NavigationControl(),
-        );
-        map.current.addControl(
-            new mapboxgl.GeolocateControl({
-                positionOptions: {
+        // Add location search control.
+        const locationSearch = new MapboxGeocoder({ accessToken: mapboxgl.accessToken});
+        map.current.addControl( locationSearch, 'top-left' );
+
+        // Add navigation controls.
+        const navigationControl = new mapboxgl.NavigationControl();
+        map.current.addControl( navigationControl );
+
+        // Add geolocate controls.
+        const geolocateControl = new mapboxgl.GeolocateControl({
+            positionOptions: {
                 enableHighAccuracy: true
             },
             // When active the map will receive updates to the device's location as it changes.
             trackUserLocation: true,
             // Draw an arrow next to the location dot to indicate which direction the device is heading.
             showUserHeading: true
-            })
-        );
-
+        });
+        map.current.addControl( geolocateControl );
     });
 
     return (
-        <>
-            <div ref={mapContainer} className="map-container" />
-        </>
+        <div ref={mapContainer} className="map-container relative">
+            <div className='absolute left-0 bottom-16 z-20'>
+                <img src={MenuIcon} />
+            </div>
+            <div className='absolute z-20 bottom-0 w-full bg-royal-blue text-white'>
+                <ul className='flex justify-around'>
+                    <li className='inline-block'><button className='py-5 px-5' title='Invite'>Invite</button></li>
+                    <li className='inline-block'><button className='py-5 px-5' title='Routes'>Routes</button></li>
+                    <li className='inline-block'><button className='py-5 px-5' title='Riders'>Riders</button></li>
+                    <li className='inline-block'><button className='py-5 px-5' title='Settings'>Settings</button></li>
+                </ul>
+            </div>
+
+        </div>
     )
 }
 
